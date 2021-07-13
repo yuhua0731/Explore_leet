6efb37010d5544311864a41b524664b31c381b05
yuhua <yuhua364@gmail.com>
2021-07-13 15:24:12 +0800

    optimized dp algorithm for Longest Increasing Subsequence

--- solution.py
+++ solution.py
@@ -43,6 +43,7 @@ class Solution:
 
     def lengthOfLIS(self, nums: List[int]) -> int:
         # DFS, failed TLE
+
         # n = len(nums)
         # global res
         # res = 0
@@ -54,7 +55,6 @@ class Solution:
         #                 search_next(i + 1, nums[i], len + 1)
         #     res = max(res, len)
         #     return
-        
         # search_next(0, - 10 ** 4 - 1, 0)
         # return res
 
@@ -62,13 +62,33 @@ class Solution:
         # dp[i] represents for LIS end with nums[i]
         # time complexity = O(n^2)
         # space complexity = O(n)
-        n = len(nums)
-        dp = [1] * n
-        for i in range(n):
-            for j in range(i + 1, n):
-                if nums[i] < nums[j]:
-                    dp[j] = max(dp[j], dp[i] + 1)
-        return max(dp)
-        
+
+        # n = len(nums)
+        # dp = [1] * n
+        # for i in range(n):
+        #     for j in range(i + 1, n):
+        #         if nums[i] < nums[j]:
+        #             dp[j] = max(dp[j], dp[i] + 1)
+        # return max(dp)
+
+        # optimized dp algorithm
+        # time complexity = O(nlogn)
+        # space complexity = O(n)
+        # beats 80+% and 90+%
+        tails = list()
+        tails.append(nums[0])
+        for i in range(1, len(nums)):
+            if nums[i] > tails[-1]:
+                tails.append(nums[i])
+            else:
+                left, right = 0, len(tails) - 1
+                while left != right:
+                    middle = (left + right) // 2
+                    if tails[middle] < nums[i]:
+                        left = middle + 1
+                    else:
+                        right = middle
+                tails[left] = min(tails[left], nums[i])
+        return len(tails)        
 
 
