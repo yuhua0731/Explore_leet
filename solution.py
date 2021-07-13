813450bc28e5693dc209933cb112ebfc4f293380
yuhua <yuhua364@gmail.com>
2021-07-13 12:02:53 +0800

    Isomorphic Strings better solution

--- solution.py
+++ solution.py
@@ -5,19 +5,13 @@ class Solution:
 
 
     def isIsomorphic(self, s: str, t: str) -> bool:
-        def findOccurance(ss: str):
-            res = dict()
-            for i in range(len(ss)):
-                if ss[i] not in res:
-                    res[ss[i]] = list()
-                res[ss[i]].append(i)
-            return res
-
-        if len(s) != len(t):
-            return False
-        dicts, dictt = findOccurance(s), findOccurance(t)
-        for value in dicts.values():
-            valuet = dictt[t[value[0]]]
-            if value != valuet:
-                return False
+        mappings = dict()
+        for i in range(len(s)):
+            if s[i] in mappings:
+                if mappings[s[i]] != t[i]:
+                    return False
+            else:
+                if t[i] in mappings.values():
+                    return False
+                mappings[s[i]] = t[i]
         return True
