fd40e2725d9c8204067691c41f461f2d7312c707
yuhua <yuhua364@gmail.com>
2021-07-13 13:46:49 +0800

    decode ways II beautiful solution

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from solution import Solution
 
 def main():
     solu = Solution()
-    print(solu.isIsomorphic('apkle', 'heelo'))
+    print(solu.isIsomorphic('apple', 'heelo'))
+    print(solu.numDecodings('1234**01*0'))
 
 if __name__ == "__main__":
     main()
--- solution.py
+++ solution.py
@@ -15,3 +15,24 @@ class Solution:
                     return False
                 mappings[s[i]] = t[i]
         return True
+
+
+    def numDecodings(self, s: str) -> int:
+        """
+        dp[0] represents for ending with one digit number
+        dp[1] represents for total decodings when last digit is '1'
+        dp[2] represents for total decodings when last digit is '2'
+        """
+        mod = 10 ** 9 + 7
+        dp, dp_new = [1, 0, 0], [0] * 3
+        for c in s:
+            if c == '*':
+                dp_new[0] = 9 * dp[0] + 9 * dp[1] + 6 * dp[2]
+                dp_new[1] = dp[0]
+                dp_new[2] = dp[0]
+            else:
+                dp_new[0] = (c != '0') * dp[0] + dp[1] + (int(c) < 7) * dp[2]
+                dp_new[1] = (c == '1') * dp[0]
+                dp_new[2] = (c == '2') * dp[0]
+            dp = [i % mod for i in dp_new]
+        return dp[0]
